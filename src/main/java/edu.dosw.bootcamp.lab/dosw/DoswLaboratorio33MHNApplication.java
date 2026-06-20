951902b0bbd08729f2fef04753a8a9f4fb8b2f66
heverthisday <heverbarrerabatero3@gmail.com>
2026-06-20 10:29:12 -0500

    config

---
+++ src/main/java/edu.dosw.bootcamp.lab/dosw/DoswLaboratorio33MHNApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.DOSW_Laboratorio3_3_M_H_N;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoswLaboratorio33MHNApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoswLaboratorio33MHNApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dosw/lab/DOSW_Laboratorio3_3_M_H_N/DoswLaboratorio33MHNApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.DOSW_Laboratorio3_3_M_H_N;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoswLaboratorio33MHNApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
